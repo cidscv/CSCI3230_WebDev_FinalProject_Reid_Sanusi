366d8b7cfbf283aa4bf3c83d2dc6eccb8ea64c3d
Owen Reid <owen.reid2@ontariotechu.net>
2022-04-21 22:40:54 -0400

    kinda done restaurant page

--- OrderUp/src/router/index.js
+++ OrderUp/src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
+import AppHeader from "../components/AppHeader.vue";
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import RestaurantView from "../views/RestaurantView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,19 +13,26 @@ const router = createRouter({
       name: "home",
       component: HomeView,
       meta: { header: true },
-    },{
-      path: '/login',
-      name: 'login',
+    },
+    {
+      path: "/login",
+      name: "login",
       component: LoginView,
       meta: { header: false },
-    },{
-      path: '/register',
-      name: 'register',
+    },
+    {
+      path: "/register",
+      name: "register",
       component: RegisterView,
+      meta: { header: false },
+    },
+    {
+      path: "/McDonalds",
+      name: "restaurantpage",
+      component: RestaurantView,
       meta: { header: false },
     },
   ],
 });
-
 
 export default router;
