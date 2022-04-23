29fe5ec07a8d34a3d0cb8f3c494371913d58d003
SanusiD <olawale.san@gmailcom>
2022-04-22 21:13:56 -0400

    Finished Account Page

--- OrderUp/src/components/Account.vue
+++ OrderUp/src/components/Account.vue
@@ -5,30 +5,40 @@
         <p>You can manage your orders and information here.</p>
 
         <div class="navigations">
-            <button @click="order"   class="orders">Orders</button>
-            <button @click="info"   class="info">Personal information</button>
-            <button @click="paydets"   class="payments">Payment Details</button>
+            <button @click="visDivs('orders')" class="orders">Orders</button>
+            <button @click="visDivs('info')" class="info">Personal information</button>
+            <button @click="visDivs('payments');" class="payments">Payment Details</button>
 
         </div>
     </div>
-    <div>
-        <div class="orders">
+    <div class="settings">
+        <div v-if="orderVisible" class="orders">
             <table>
                 <thead>
-                    <td>Order ID</td>
-                    <td>Date</td>
-                    <td>Total</td>
-                    <td>Action</td>
+                    <tr class="head">
+                        <th>Order ID</th>
+                        <th>Date</th>
+                        <th>Total</th>
+                        <th>Action</th>
+                    </tr>
                 </thead>
                 <tbody>
-                    <td>1</td>
-                    <td>May 3rd, 2022</td>
-                    <td>$332.68</td>
-                    <td><Button>View</Button></td>
+                    <tr>
+                        <td>1</td>
+                        <td>May 3rd, 2022</td>
+                        <td>$332.68</td>
+                        <td><button>View</button></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>May 3rd, 2022</td>
+                        <td>$332.68</td>
+                        <td><button>View</button></td>
+                    </tr>
                 </tbody>
             </table>
         </div>
-        <div class="personalInfo">
+        <div v-if="infoVisible" class="personalInfo">
             <form>
                 <div class="split left">
                     <p>First Name:</p>
@@ -63,26 +73,25 @@
                 </div>
                 
                
-                <button class="btn"> SHOOT YOUR MESSAGE </button>
+                <button class="btn"> Update </button>
             </form>
         </div>
-
-        <div class="card">
+        <div v-if="cardVisible" class="card">
             <form>
+                 <div class="split left">
                     <p>Card Number:</p>
-                    <input type="text"  name="firstName" placeholder="firstName" required/>
+                    <input type="text"  name="cardNum" placeholder="374245455400126" required/>
                     <br>
                     <p>Exp Date:</p>
-                    <input type="text"  name="Number" placeholder="Number" required/>
+                    <input type="text"  name="expDate" placeholder="10/26" required/>
                     <br>
-                    <p>City:</p>
-                    <input type="text"  name="City" placeholder="City" required/>
-                    <br>
+                </div>
+                <div class="split left">  
                     <p>Cvv:</p>
-                    <input type="text"  name="Postal Code" placeholder="Postal Code" required/>
+                    <input type="text"  name="cvv" placeholder="321" required/>
                     <br>
-               
-                <button class="btn"> SHOOT YOUR MESSAGE </button>
+                </div>      
+                <button class="btn"> Update </button>                         
             </form>
         </div>
     </div>
@@ -99,7 +108,11 @@
 }
 .detail{
     background-color: beige;
+    padding-top: 40px;
     height: 20vh;
+    h1{
+        margin-top: 0;
+    }
     .navigations{
         display: flex;
         align-content: center;
@@ -111,12 +124,149 @@
             color: white;
             border: none;
             z-index: 9999;
+            
         }
         button:hover{
             background-color: rgb(32, 139, 32);
             cursor: pointer;
         }
+        .selected{
+            background-color: rgb(32, 139, 32);
+        }
     }
+}
+
+.settings{
+    .orders{
+        margin: 40px;
+
+        table{
+            text-align: left;
+            margin: 0 auto;
+            border-collapse: collapse;
+            width: 80vw;
+            padding: 40px;
+            color: darkgreen;
+            border-radius: 15px 50px 30px 5px;
+            tr{
+                border: 2px solid rgb(230, 230, 230);
+
+                th{
+                    padding: 20px;
+                }
+            }
+            tbody tr{
+                border: 2px solid rgb(230, 230, 230);
+                td{
+                    padding: 20px;
+                }
+            }
+            tbody tr:nth-child(even){
+                background-color: rgb(230, 230, 230);
+            }
+            button{
+                background-color: darkgreen;
+                width: 60px;
+                height:30px;
+                color: white;
+                border: none;
+                
+            }
+            button:hover{
+                background-color: rgb(32, 139, 32);
+                cursor: pointer;
+            }
+        }
+    }
+    .personalInfo{
+        form{
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr;
+            padding: 40px;
+            text-align: left;
+            input{
+                background-color: white;
+                width: 80%;
+                height: 40px;
+                border: none;
+                outline: none;
+                border-bottom:1px solid rgb(0, 46, 0);
+                transition: border-bottom 0.1s;
+
+            }
+            input:hover{
+                border-bottom: 2px solid rgb(46, 133, 46);
+            }
+            input:active{
+                border-bottom: 2px solid rgb(46, 133, 46);
+
+            }
+            p{
+                font-size: 12pt;
+                font-weight: 600;
+                margin: 20px 0px 10px 0px;
+            }
+            button{
+                margin-top: 20px;
+                background-color: darkgreen;
+                width: 60px;
+                height:30px;
+                color: white;
+                border: none;
+                
+            }
+            button:hover{
+                background-color: rgb(32, 139, 32);
+                cursor: pointer;
+            }
+        }
+    }
+    .card{
+        form{
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr;
+            padding: 40px;
+            text-align: left;
+            input{
+                background-color: white;
+                width: 80%;
+                height: 40px;
+                border: none;
+                outline: none;
+                border-bottom:1px solid rgb(0, 46, 0);
+                transition: border-bottom 0.1s;
+
+            }
+            input:hover{
+                border-bottom: 2px solid rgb(46, 133, 46);
+            }
+            input:active{
+                border-bottom: 2px solid rgb(46, 133, 46);
+
+            }
+            p{
+                font-size: 12pt;
+                font-weight: 600;
+                margin: 20px 0px 10px 0px;
+            }
+            button{
+                margin-top: 20px;
+                background-color: darkgreen;
+                width: 60px;
+                height:30px;
+                color: white;
+                border: none;
+                
+            }
+            button:hover{
+                background-color: rgb(32, 139, 32);
+                cursor: pointer;
+            }
+        }
+    }
+
 }
 
 </style>
@@ -127,40 +277,37 @@
     export default {
         name: "Account",
         data(){
-            return{
-                username: "Bret",
-                password: "hildegard.org",
-                errorMessage: ""
+            return{ 
+                 orderVisible: false,
+                 infoVisible: false,
+                 cardVisible: false,
             }
         },
         methods: {
-            
-            order: function(){
-                $('.orders').click(function(){
-                    $('.orders').show()
-                    $('.personalInfo').hide()
-                    $('.card').hide()
-                })
-            },
-            info: function(){
-                $('.personalInfo').click(function(){
-                    console.log('personalInfo')
+            visDivs: function(ev){
+                this.orderVisible = false
+                this.infoVisible = false
+                this.cardVisible = false
+                $('.orders').removeClass('selected');
+                $('.info').removeClass('selected');
+                $('.payments').removeClass('selected');
 
-                    $('.personalInfo').show()
-                    $('.orders').hide()
-                    $('.card').hide()
-                })
-            },
-            paydets: function(){
-                $('.card').click(function(){
-                    console.log('card')
+                 
+                if(ev == 'orders'){
+                    $('.orders').addClass('selected');
+                    this.orderVisible = true
+                }
+                if(ev == 'info'){
+                    $('.info').addClass('selected');
+                    this.infoVisible = true
 
-                    $('.card').show()
-                    $('.orders').hide()
-                    $('.personalInfo').hide()
-                })
-            },
-           
+                }
+                if(ev == 'payments'){
+                    $('.payments').addClass('selected');
+                    this.cardVisible = true
+
+                }
+            },           
         },
     }
 </script>
