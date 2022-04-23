270099c0716b5b71e42a17eaf560cfd532b998d6
Owen Reid <55000504+cidscv@users.noreply.github.com>
2022-04-23 14:55:34 -0400

    server

--- api/server.js
+++ api/server.js
@@ -5,14 +5,6 @@ const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://root:Password1.@orderup.cmbes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-/*
-client.connect((err, client) => {
-    if (err) return console.error(err)
-    console.log('Connected to Database')
-  //const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  //client.close();
-});*/
 
 client.connect().then(client => {
     console.log('Connected to Database')
