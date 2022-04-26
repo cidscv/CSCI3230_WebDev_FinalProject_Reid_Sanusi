a0f3582c1a310715c73a037750e0d235c34cf02b
SanusiD <olawale.san@gmailcom>
2022-04-26 18:48:51 -0400

    done cart. Almost done the full functionality

--- OrderUp/src/components/AppHeader.vue
+++ OrderUp/src/components/AppHeader.vue
@@ -8,36 +8,105 @@
         <router-link to="/restaurants">Restaurants</router-link>
         <router-link to="/contact">Contact Us</router-link>
         <router-link to="/login" v-if="!isLoggedIn">Login</router-link>
-        <div v-if="isLoggedIn" id="whenloggedin">
-            <router-link style="text-decoration: none;" to="/account" v-if="isLoggedIn">Profile</router-link> 
-            <router-link v-on:click="logout" to="/" v-if="isLoggedIn">Logout</router-link> 
-        </div>
+        <router-link style="text-decoration: none;" to="/account" v-if="isLoggedIn">Profile</router-link> 
+        <router-link v-on:click="logout" to="/" v-if="isLoggedIn">Logout</router-link> 
+        <router-link to="#" >    
+            <img
+            src="../assets/shopping-cart.svg"
+            alt="shopping cart" @click="toggleCart" v-if="isLoggedIn" class="cartButton js-toggle-cart" width="20px"/>
+        </router-link>
     </div>
 </div>
+<aside class="cart js-cart">
+        <div class="cart__header">
+          <h1 class="cart__title">Shopping cart</h1>
+          <p class="cart__text">
+            <a @click="toggleCart" class="button button--light js-toggle-cart" href="#" title="Close cart">
+              Close cart
+            </a>
+          </p>
+        </div>
+        <div class="cart__products js-cart-products">
+          <p class="cart__empty js-cart-empty">
+            Add a product to your cart
+          </p>
+          <div class="cart__product js-cart-product-template">
+            <article class="js-cart-product">
+              <h1>Product title</h1>
+              <p>
+                <a @click="removeProduct" class="js-remove-product" href="#" title="Delete product">
+                  Delete product
+                </a>
+              </p>
+            </article>
+          </div>
+        </div>
+        <div class="cart__footer">
+          <p class="cart__text">
+            <a class="button" href="./order.html" title="Buy products">
+              Buy products
+            </a>
+          </p>
+        </div>
+      </aside>
 </template>
 
 <script>
 import auth from "../js/auth"
+import $ from "JQuery"
 
-export default{
+export default {
     name: 'AppHeader',
     data(){
         return{
             isLoggedIn: auth.isLoggedIn(),
+            cartOpen:false,
+            numberOfProducts:0
         };
     },
-
     created(){
         auth.onLoginStatus = isLoggedIn => {
             this.isLoggedIn = isLoggedIn;
         }
     },
-    methods:{
+    methods: {
         logout: function(){
             auth.logout( (res) =>{
                 console.log(res)
             });
-        }
+        },
+        toggleCart: function(e) {
+            e.preventDefault();
+            if(this.cartOpen) {
+                this.closeCart();
+                return;
+            }
+            this.openCart();
+        },
+        addProduct: function(e) {
+            e.preventDefault();
+            this.openCart();
+            $('.js-cart-empty').addClass('hide');
+            var product = $('.js-cart-product-template').html();
+            $('.js-cart-products').prepend(product);
+            this.numberOfProducts++;
+        },
+        removeProduct:function(e){
+            e.preventDefault();
+            this.numberOfProducts--;
+            $(this).closest('.js-cart-product').hide(250);
+            if(this.numberOfProducts == 0) {
+                $('.js-cart-empty').removeClass('hide');
+            }
+        },
+        openCart: function() {
+            this.cartOpen = true;
+            $('body').addClass('open');
+        },
+        closeCart: function() {
+            this.cartOpen = false;
+            $('body').removeClass('open');
+        },
     }
 }
 </script>
@@ -74,6 +143,139 @@ export default{
     color: #0F4900;
     border-bottom: 2px solid #59da38;
 }
+.navigation .links .cartButton{
+        color: #0F4900;
+        width: 20px;
+    }
     
+body.open {
+    overflow-y: hidden;
+}
+
+.button {
+    display: inline-block;
+    background: #39c;
+    padding: 5px 10px;
+    border-radius: 3px;
+    font-weight: bold;
+    font-size: 14px;
+    text-decoration: none;
+    color: #fff;
+}
+.button--light {
+    background: #fff;
+    color: #f00;
+}
+.cart {
+    position: fixed;
+    background: #f3f3f3;
+    width: 400px;
+    max-width: 90%;
+    height: 100%;
+    top: 0;
+    right: 0;
+    box-shadow: -2px 0 4px rgba(0, 0, 0, 0.2);
+    overflow: hidden;
+    transform: translate(500px, 0);
+    transition: transform 250ms ease-in-out;
+    z-index: 20;
+}
+body.open .cart {
+    transform: translate(0, 0);
+}
+.cart__header {
+    box-sizing: border-box;
+    position: absolute;
+    background: rgba(255, 255, 255, 0.9);
+    width: 100%;
+    padding: 15px 15px;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    z-index: 2;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+.cart__header .cart__text {
+    float: right;
+}
+.cart__title {
+    font-size: 20px;
+    line-height: 40px;
+    margin: 0;
+    float: left;
+}
+.cart__products {
+    box-sizing: border-box;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    padding: 70px 0;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    -webkit-overflow-scrolling: touch;
+    z-index: 1;
+}
+.cart__products article {
+    padding: 15px;
+    border-bottom: 1px dotted #ccc;
+}
+.cart__products article h1 {
+    font-size: 16px;
+    line-height: 20px;
+    margin: 0;
+}
+.cart__products article p {
+    font-size: 14px;
+    margin: 0;
+}
+.cart__products article p a {
+    color: #f00;
+}
+.cart__product {
+    display: none;
+}
+.cart__empty {
+    padding: 30px 15px;
+    margin: 0;
+    font-style: italic;
+    text-align: center;
+}
+.cart__empty.hide {
+    display: none;
+}
+.cart__footer {
+    box-sizing: border-box;
+    position: absolute;
+    background: rgba(255, 255, 255, 0.9);
+    width: 100%;
+    padding: 15px;
+    left: 0;
+    bottom: 0;
+    z-index: 2;
+    box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.2);
+    text-align: right;
+}
+.cart__text {
+    margin: 0;
+}
+.cart__text .button {
+    padding: 10px 15px;
+}
+.lightbox {
+    position: fixed;
+    background: #000;
+    width: 0;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    opacity: 0;
+    transition: opacity 250ms ease-in-out, width 0ms ease-in-out 250ms;
+}
+body.open .lightbox {
+    width: 100%;
+    opacity: 0.8;
+    transition: opacity 250ms ease-in-out, width 0ms ease-in-out;
+}
 
 </style>
